38c99fb6f4fea9e5ae7ac713a7082d4ba9a8f506
Andrew Dunn <dunnand@oregonstate.edu>
2021-07-06 22:22:24 -0500

    Added CV Display Header, started outlining page for future PDF implementation

--- src/components/PersonalInfoDisplay.js
+++ src/components/PersonalInfoDisplay.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
 import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { mapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 function PersonalInfoDisplay(props) {
 
     return (
         <BackgroundDiv>
-            <FormatDiv>
+            <HeaderDiv>
                 <StyledH1>{props.values.firstName} {props.values.lastName}</StyledH1>
                 <StyledH2>{props.values.title}</StyledH2>
                 <p>{props.values.description}</p>
-            </FormatDiv>
+            </HeaderDiv>
+            <ContactDiv>
+                <p><span>&#9993;</span> {props.values.email}</p>
+                <FontAwesomeIcon icon={mapMarkerAlt}/>
+            </ContactDiv>
         </BackgroundDiv>
 
     )
@@ -18,26 +24,37 @@ function PersonalInfoDisplay(props) {
 
 const BackgroundDiv = styled.div`
     text-align: left;
-    width: 8.5in;
-    height: 1.5in;
-    background: #14557B;
+    font-family: Garamond;
 `; 
 
-const FormatDiv = styled.div`
+const HeaderDiv = styled.div`
     text-align: left;
-    background: #14557B;
+    background: #4D774E;
     color: white;
-    padding-left: 20px
-
+    padding-left: 20px;
+    padding-top: 1px;
+    height: 1.5in;
 `; 
 
+const ContactDiv = styled.div`
+    text-align: left;
+    background: #164A41;
+    color: white;
+    height: 1in;
+    display: grid;
+    grid-template-columns: repeat(2, )
+    padding-left: 20px;
+`; 
 
 const StyledH1 = styled.h1`
     margin-bottom: 0px;
+    font-size: 36px;
 `;
 
 const StyledH2 = styled.h2`
-    margin-top: 5px;
+    margin-top: 0px;
+    padding-top: 0px;
+    font-size: 24px;
 `;
 
 export default PersonalInfoDisplay;
